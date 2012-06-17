996b8e460610217609255a8fd0e097d45424a146
zhaohuabing <zhaohuabing@gmail.com>
2012-06-17 10:26:36 +0800

    refactor

--- src/measure/MeasureUtil.java
+++ src/measure/MeasureUtil.java
@@ -11,36 +11,13 @@ public class MeasureUtil
 
     public static int compare(Measure m1, Measure m2)
     {
-        return getValueInMinimumUnit(m2.getValue(), m2.getClass().getName())
-                - getValueInMinimumUnit(m1.getValue(), m1.getClass().getName());
-    }
-
-    public static int getValueInMinimumUnit(int value, String measureClassName)
-    {
-        int unitValue = value;
-        TableEntry[] tableEntry = measureTable.getTableEntry();
-        for (int i = 0; i < tableEntry.length; i++)
-        {
-            if (!tableEntry[i].measureClassName.equals(measureClassName))
-            {
-                unitValue = unitValue * tableEntry[i].basicValue;
-            }
-            else
-            {
-                return unitValue;
-            }
-        }
-        return unitValue;
+        return getValueInMinimumUnit(m2) - getValueInMinimumUnit(m1);
     }
 
     public static Measure add(Measure m1, Measure m2)
     {
         //相加后的返回值使用两者中大的度量衡,因此先获取较大的度量衡
         Measure biggerMeasure = getBiggerMeasure(m1, m2);
-
-        int sumInMinimunUnit = getValueInMinimumUnit(m1.getValue(), m1.getClass().getName())
-                + getValueInMinimumUnit(m2.getValue(), m2.getClass().getName());
-        int oneUnitValueInMinimumUnit = getValueInMinimumUnit(1, biggerMeasure.getClass().getName());
 
         Measure result;
         try
@@ -51,22 +28,14 @@ public class MeasureUtil
         {
             throw new RuntimeException("", e);
         }
-        result.setValue(sumInMinimunUnit / oneUnitValueInMinimumUnit);
+        result.setValue(getSumInMinimunUnit(m1, m2) / getOneUnitValueInMinimumUnit(biggerMeasure));
 
         return result;
     }
 
-    private static Measure getBiggerMeasure(Measure m1, Measure m2)
+    private static int getSumInMinimunUnit(Measure m1, Measure m2)
     {
-        if (measureTable.getMeasureIndex(m1) > measureTable.getMeasureIndex(m2))
-        {
-            return m1;
-        }
-        else
-        {
-            return m2;
-        }
-
+        return getValueInMinimumUnit(m1) + getValueInMinimumUnit(m2);
     }
 
     public static String getDisplayString(Measure m)
@@ -75,8 +44,7 @@ public class MeasureUtil
 
         TableEntry[] tableEntry = measureTable.getTableEntry();
 
-        displayStr = getDisplayStrDealing(displayStr, getValueInMinimumUnit(m.getValue(), m.getClass().getName()),
-                tableEntry.length - 1);
+        displayStr = getDisplayStrDealing(displayStr, getValueInMinimumUnit(m), tableEntry.length - 1);
         if (displayStr.equals(""))
         {
             displayStr = "0 " + measureTable.getMeasureDisplayName(m.getClass().getName());
@@ -93,8 +61,8 @@ public class MeasureUtil
         TableEntry[] tableEntry = measureTable.getTableEntry();
         for (int i = index; i >= 1;)
         {
-            quotient = length / getValueInMinimumUnit(1, tableEntry[i].measureClassName);
-            extra = length % getValueInMinimumUnit(1, tableEntry[i].measureClassName);
+            quotient = length / getOneUnitValueInMinimumUnit(tableEntry[i].measureClassName);
+            extra = length % getOneUnitValueInMinimumUnit(tableEntry[i].measureClassName);
 
             if (quotient > 0)
             {
@@ -126,9 +94,54 @@ public class MeasureUtil
     public static String getDisplayString(Measure m, String displayMeasureClassName)
     {
 
-        return getValueInMinimumUnit(m.getValue(), m.getClass().getName())
-                / getValueInMinimumUnit(1, displayMeasureClassName) + " "
+        return getValueInMinimumUnit(m) / getOneUnitValueInMinimumUnit(displayMeasureClassName) + " "
                 + measureTable.getMeasureDisplayName(displayMeasureClassName);
+
+    }
+
+    private static int getOneUnitValueInMinimumUnit(Measure biggerMeasure)
+    {
+        return getValueInMinimumUnit(1, biggerMeasure.getClass().getName());
+    }
+
+    private static int getOneUnitValueInMinimumUnit(String measureClassName)
+    {
+        return getValueInMinimumUnit(1, measureClassName);
+    }
+
+    private static int getValueInMinimumUnit(Measure m)
+    {
+        return getValueInMinimumUnit(m.getValue(), m.getClass().getName());
+    }
+
+    private static int getValueInMinimumUnit(int value, String measureClassName)
+    {
+        int unitValue = value;
+        TableEntry[] tableEntry = measureTable.getTableEntry();
+        for (int i = 0; i < tableEntry.length; i++)
+        {
+            if (!tableEntry[i].measureClassName.equals(measureClassName))
+            {
+                unitValue = unitValue * tableEntry[i].basicValue;
+            }
+            else
+            {
+                return unitValue;
+            }
+        }
+        return unitValue;
+    }
+
+    private static Measure getBiggerMeasure(Measure m1, Measure m2)
+    {
+        if (measureTable.getMeasureIndex(m1) > measureTable.getMeasureIndex(m2))
+        {
+            return m1;
+        }
+        else
+        {
+            return m2;
+        }
 
     }
 }
